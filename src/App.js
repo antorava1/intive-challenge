925c6d43ff940d1500293b3216ab0d74f204966d
Antonella Ravaioli <5211016-antorava1@users.noreply.gitlab.com>
2021-07-28 21:07:01 -0300

    #3

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { useState, useEffect} from 'react';
 //import { withNamespaces } from 'react-i18next';
 import AppBar from '@material-ui/core/AppBar';
-import FormGroup from '@material-ui/core/FormGroup';
-import Input from '@material-ui/core/Input';
 import Button from '@material-ui/core/Button';
 import axios from 'axios';
 import InfoCard from './components/Cards/InfoCard';
+import { ButtonGroup } from 'react-bootstrap';
+import ReactPaginate from 'react-paginate';
 //import i18n from './i18n';
 
 
@@ -13,8 +13,42 @@ import InfoCard from './components/Cards/InfoCard';
   const App = () => {
     const [users, setUsers] = useState([]);
 
+    const [pageNumber, setPageNumber] = useState(0);
+    const usersPerPage = 20;
+    const pagesVisited = pageNumber * usersPerPage;
+
+    const pageCount = Math.ceil(
+      users.length / usersPerPage
+    );
+
+    const changePage = ({selected}) =>  {
+      setPageNumber(selected)
+    };
+
+    const showUsers = users.slice(pagesVisited, pagesVisited + usersPerPage).map(
+      (results, index) => {
+        return(
+          <InfoCard
+            firstName={results.name.first}
+            lastName={results.name.last}
+            image={results.picture.large}
+            city={results.location.city}
+            country={results.location.country}
+            gender={results.gender}
+            age={results.dob.age}
+            birthdate={results.dob.date}
+            phone={results.phone}
+            cell={results.cell}
+            nationality={results.nat}
+            username={results.login.username} 
+            email={results.email}
+            key={index}
+          />
+        )
+      })
+
     const getUsers = () => {
-      axios.get('https://randomuser.me/api/?results=20')
+      axios.get('https://randomuser.me/api/?results=500')
       .then((response) => {
         console.log(response)
         setUsers(response.data.results)
@@ -30,41 +64,33 @@ import InfoCard from './components/Cards/InfoCard';
       getUsers()
   }, [])
 
-
   return (
     <>
-      <AppBar class="navbar navbar-expand-lg navbar-light bg-secondary">
+      <AppBar class="navbar navbar-expand-lg navbar-dark bg-primary">
         <div className="container-fluid">
           <h3 className= "navbar-brand">Intive - Challenge</h3>
+          <ButtonGroup className="d-flex">
+              <Button class="btn btn-secondary btn-sm my-2 my-sm-1" style={{"marginRight" : "5px"}} /* onClick={() => changeLanguage('es')} */>spa</Button>
+            <Button class="btn btn-danger btn-sm my-2 my-sm-1" /* onClick={() => changeLanguage('es')} */>eng</Button>
+          </ButtonGroup>
         </div>
       </AppBar> 
       <div className="container">
-        {/* <div class="btn-group" role="group">
-          <Button class="btn btn-primary btn-sm my-2 my-sm-1" onClick={() => changeLanguage('es')}>{t('esp')}</Button>
-          <Button class="btn btn-secondary btn-sm my-2 my-sm-1" onClick={() => changeLanguage('en')}>{t('eng')}</Button>
-        </div> */}
         <div className="row">
-          {users.map((results, index) => 
-            <InfoCard
-              firstName={results.name.first}
-              lastName={results.name.last}
-              image={results.picture.large}
-              city={results.location.city}
-              country={results.location.country}
-              gender={results.gender}
-              age={results.dob.age}
-              birthdate={results.dob.date}
-              phone={results.phone}
-              cell={results.cell}
-              nationality={results.nat}
-              username={results.login.username} 
-              email={results.email}
-              key={index}
-            />
-          )}
+          {showUsers}
         </div>
         <div className="d-flex justify-content-center">
-        {/* PAGINATION */}
+          <ReactPaginate class="pagination"
+            previousLabel={"Prev"}
+            nextLabel={"Next"}
+            pageCount={pageCount}
+            onPageChange={changePage}
+            containerClassName={"pagination"}
+            previousLinkClassName={"page-link"}
+            nextLinkClassName={"page-link"}
+            activeClassName={"page-item active"}
+            pageLinkClassName={"page-link"}
+          />
         </div>
       </div>
     </>
