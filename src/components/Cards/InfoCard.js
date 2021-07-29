c5e58bbee5f3087b979b031713f440501ed228b0
Antonella Ravaioli <5211016-antorava1@users.noreply.gitlab.com>
2021-07-28 21:07:35 -0300

    #1

--- src/components/Cards/InfoCard.js
+++ src/components/Cards/InfoCard.js
@@ -7,15 +7,14 @@ const InfoCard = ({id, firstName, lastName, username, image, city, country, emai
     return (
 
         <div className="col-md-3" key={id}>
-            <div className="card border-primary m-3" style={{'height':"21rem"}}>
+            <div className="card border-dark m-3" style={{'height':"21rem"}}>
                 <div className="card-img-top d-flex justify-content-center align-items-center" style={{'minHeight':"200px", 'maxHeight':"200px"}}>
                     <img src={image} alt={firstName} className="w-75 h-50"/>
                 </div>
             <div className="card-body row">
-                <h5 className="card-title text-center">{firstName} {lastName}</h5>
+                <h5 className="card-title text-center text-danger" onClick={() => setOpenModal(true)} style={{"cursor" : "pointer"}}>{firstName} {lastName}</h5>
                 <h6 className="card-title text-center">{city}, {country}</h6>
             </div>
-            <button onClick={() => setOpenModal(true)} className="btn btn-primary btn-sm" style={{"marginLeft" : "30px"}}>Detalles</button>
             {openModal && 
                 <UserInfoModal 
                     closeModal={setOpenModal} 
